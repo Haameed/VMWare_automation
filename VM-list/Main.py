8f2f3afb9205d74566c9a732db9c9fc98852d130
Haameed <hamed.maaleki@gmail.com>
2022-01-23 14:41:20 +0330

    Update Main.py

--- VM-list/Main.py
+++ VM-list/Main.py
@@ -2,28 +2,40 @@ import connector
 import secret
 import pandas as pd
 import smtplib
+from pyVmomi import vim
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
 from email import encoders
+
 from datetime import datetime
 
 
-
-sender_email = automation@mycompany.com
 file_name = f'vm_list_{datetime.now().strftime("%Y-%m-%d")}.xlsx'
 def vm_list():
-    vcenters = ["vcsa-hq", "vcsa-aboard", "vcsa-dc", "vcsa-cc"]
+    vcenters = ["vcsa-1", "vcsa-2", "vcsa-3", "vcsa-4"]
     writer = pd.ExcelWriter(file_name, engine='xlsxwriter')
     dataframe = {}
     for vcenter in vcenters:
+        # dataframe[vcenter] = {"DataCenter": [], "Cluster": [], "Host": [], "Name": [], "ID": [],
+        #     "Power_status": [], "Is_template": [], "Number of CPUs": [],
+        #     "CPU_Cores": [], "Total CPU": [], "CPU Hot_add": [], "Memory GB": [], "Memory Hot_add": [],
+        #     "VM_Tools Version": [], "virtual_disks": [], "datastore_name": [], "VM Path": [],
+        #     "Network Count": [], "network_name": [], "tools_installType":[], "tools_runningStatus": [],
+        #     "tools_status": [], "tools_version": [], "tools_versionStatus": [], "tools_versionStatus2": [],
+        #     "vm_guestFamilly": [], "vm_guestHostname": [], "vm_hwVersion": [], "vm_ip": [], "vm_ips": []}
         dataframe[vcenter] = {"DataCenter": [], "Cluster": [], "Host": [], "Name": [], "ID": [],
-            "Power_status": [], "Is_template": [], "Number of CPUs": [],
-            "CPU_Cores": [], "Total CPU": [], "CPU Hot_add": [], "Memory GB": [], "Memory Hot_add": [],
-            "VM_Tools Version": [], "virtual_disks": [], "datastore_name": [], "VM Path": [],
-            "Network Count": [], "network_name": [], "tools_installType":[], "tools_runningStatus": [],
-            "tools_status": [], "tools_version": [], "tools_versionStatus": [], "tools_versionStatus2": [],
-            "vm_guestFamilly": [], "vm_guestHostname": [], "vm_hwVersion": [], "vm_ip": [], "vm_ips": []}
+                              "Power_status": [], "Is_template": [], "Number of CPUs": [],
+                              "CPU_Cores": [], "Total CPU": [], "CPU Hot_add": [], "Memory GB": [],
+                              "Memory Hot_add": [],
+                              "VM_Tools Version": [], "virtual_disks": [], "datastore list": [], "Ethernet list": [],
+                              "Total allocated disk": [],
+                              "Network Count": [], "networks_name": [], "tools_installType": [],
+                              "tools_runningStatus": [],
+                              "tools_status": [], "tools_version": [], "tools_versionStatus": [],
+                              "tools_versionStatus2": [],
+                              "vm_guestFamilly": [], "vm_guestHostname": [], "vm_hwVersion": [], "vm_ip": [],
+                              "vm_ips": []}
 
         connection = connector.vcenter_connector(
             server=vcenter, domain=secret.domain, user=secret.user, password=secret.pwd
@@ -40,7 +52,6 @@ def vm_list():
                 for host in hosts:
                     host_name = host.name
                     for vm in host.vm:
-                        print(vm.name)
                         vm_name = vm.name
                         cpu_num = vm.config.hardware.numCPU
                         cpu_cores = vm.config.hardware.numCoresPerSocket
@@ -51,17 +62,26 @@ def vm_list():
                         memory_hotadd = vm.config.memoryHotAddEnabled
                         power_status = vm.summary.runtime.powerState
                         is_template = vm.summary.config.template
-                        vm_path_name = vm.summary.config.vmPathName
+                        # vm_path_name = vm.summary.config.vmPathName
                         ethernet_cards = vm.summary.config.numEthernetCards
                         virtual_disks = vm.summary.config.numVirtualDisks
                         vm_id = str(vm).split(":")[1].replace("'", "")
-                        datastores_name = []
-                        for ds in vm.summary.vm.config.datastoreUrl:
-                            datastores_name.append(ds.name)
+                        datastores_list = ""
+                        ethernets_list = ""
+                        totalAllocatedDisk= 0
+                        for device in vm.config.hardware.device:
+                            if isinstance(device, vim.vm.device.VirtualDisk):
+                                datastores_list += f"({device.deviceInfo.label}, {device.backing.datastore.name}, {round(device.capacityInKB / 1024 / 1024)}),"
+                                totalAllocatedDisk = totalAllocatedDisk + device.capacityInKB
+                            elif isinstance(device, vim.vm.device.VirtualVmxnet3):
+                                ethernets_list += f"({device.deviceInfo.label}, {device.macAddress}, connected = {device.connectable.connected} , wakeOnLanEnabled = {device.wakeOnLanEnabled}),"
+                        totalAllocatedDisk = round(totalAllocatedDisk / 1024 / 1024)
+                        datastores_list = datastores_list.rstrip(",")
+                        ethernets_list = ethernets_list.rstrip(",")
                         vm_network = vm.summary.vm.network
-                        network_name = []
+                        networks_name = []
                         for net in vm_network:
-                            network_name.append(net.name)
+                            networks_name.append(net.name)
                         tools_installType = vm.guest.toolsInstallType
                         tools_runningStatus = vm.guest.toolsRunningStatus
                         tools_status = vm.guest.toolsStatus
@@ -75,9 +95,9 @@ def vm_list():
                         vm_ips = []
                         if tools_status == 'toolsOk' or tools_status == 'toolsOld':
                             for nic in vm.guest.net:
-                                mac_add = nic.macAddress
-                                for addr in nic.ipConfig.ipAddress:
-                                    vm_ips.append((addr.ipAddress, mac_add))
+                                if hasattr(nic, 'ipConfig') and hasattr(nic.ipConfig, 'ipAddress'):
+                                    for addr in nic.ipConfig.ipAddress:
+                                        vm_ips.append(addr.ipAddress)
                         else:
                             vm_ips = "none"
                         dataframe[vcenter]["DataCenter"].append(dc_name)
@@ -95,10 +115,12 @@ def vm_list():
                         dataframe[vcenter]["Memory Hot_add"].append(memory_hotadd)
                         dataframe[vcenter]["VM_Tools Version"].append(vm_tools)
                         dataframe[vcenter]["virtual_disks"].append(virtual_disks)
-                        dataframe[vcenter]["datastore_name"].append(datastores_name)
-                        dataframe[vcenter]["VM Path"].append(vm_path_name)
+                        dataframe[vcenter]["datastore list"].append(datastores_list)
+                        dataframe[vcenter]["Total allocated disk"].append(totalAllocatedDisk)
+                        # dataframe[vcenter]["VM Path"].append(vm_path_name)
                         dataframe[vcenter]["Network Count"].append(ethernet_cards)
-                        dataframe[vcenter]["network_name"].append(network_name)
+                        dataframe[vcenter]["Ethernet list"].append(ethernets_list)
+                        dataframe[vcenter]["networks_name"].append(networks_name)
                         dataframe[vcenter]["tools_installType"].append(tools_installType)
                         dataframe[vcenter]["tools_runningStatus"].append(tools_runningStatus)
                         dataframe[vcenter]["tools_status"].append(tools_status)
@@ -128,14 +150,14 @@ def vm_list():
                 series.astype(str).map(len).max(),
                 len(str(series.name))
             )) + 1
-            worksheet.set_column(idx, idx, max_len)
+            worksheet.set_column(idx, idx, 20)
         for col_num, value in enumerate(df.columns.values):
             worksheet.write(0, col_num, value, header_format)
 
     writer.save()
 
 def send_mail(contact , body):
-    sender_email = sender_email 
+    sender_email = "automation@mycompany.com"
     receiver_email = contact
 
     msg = MIMEMultipart()
@@ -148,11 +170,11 @@ def send_mail(contact , body):
     encoders.encode_base64(part)
     part.add_header('content-disposition', 'attachment', filename=file_name)
     msg.attach(part)
-    with smtplib.SMTP('webmail.mycompany.com', 25) as smtpObj:
+    with smtplib.SMTP('webmail.mycompany.ir', 25) as smtpObj:
         smtpObj.ehlo()
         smtpObj.starttls()
         smtpObj.sendmail(sender_email, receiver_email, msg.as_string())
 
 if __name__ == "__main__":
     vm_list()
-    send_mail("virtualization@mycompany.com.ir", "Dear colleagues,\n Please find th attachment for VM list from all vCenters")
+    send_mail("virtualization@mycompany.com", "Dear colleagues,\n Please find th attachment for VM list from all vCenters")
